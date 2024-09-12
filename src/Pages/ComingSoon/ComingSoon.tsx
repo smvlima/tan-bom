93810c8846cc213ecaf6a6be46785f15a1f239ce
smvlima <smvlima26@gmail.com>
2024-09-12 19:26:50 +0100

    get images correctly

--- src/Pages/ComingSoon/ComingSoon.tsx
+++ src/Pages/ComingSoon/ComingSoon.tsx
@@ -1,8 +1,30 @@
-import './ComingSoon.css';
+import "./ComingSoon.css";
 
-export const ComingSoon: React.FC<{}> = () =>  {
+export const ComingSoon: React.FC<{}> = () => {
   return (
-  <>  'ola'</>
-  );
-}
+    <div className="container">
+      <div className="highlight" />
+      <div className="logo">
+        <img
+          src={"../../assets/logo.png"}
+          alt="tan bom logo"
+          width="166px"
+          height="166px"
+        />
+      </div>
 
+      <div className="content">
+        <img
+          src={"../../assets/chase_your_best_mood.png"}
+          alt="chase your best mood"
+          width="100%"
+        />
+        <img
+          src={"../../assets/coming_soon.png"}
+          alt="coming soon"
+          width="100%"
+        />
+      </div>
+    </div>
+  );
+};
